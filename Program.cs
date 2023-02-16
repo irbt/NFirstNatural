3a49be8edd18ee1522ec3fb10eaab64a70a76c98
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2023-02-16 22:00:13 +0100

    exercice

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Write a program in C# Sharp to print the first n natural ctrber using recursion.
+namespace Program;
+
+class FirstNaturalTask{
+
+static int FirstNatural(int strval ,int ctr){
+
+if (ctr<1)
+{
+    return strval;
+}
+
+ctr--;
+Console.Write (" {0} ",strval);
+return FirstNatural (strval+1, ctr);
+
+}
+
+public static void Main (string[] args){
+
+    FirstNatural(1,150);
+}
+
+}
